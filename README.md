784cb4c02bead09980cea24ba684f18a58d9a6c0
Mihai Andrei <mihai.andrei@modex.tech>
2021-11-17 18:26:02 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Express API Starter
+
+This api starter is based on https://github.com/w3cj/express-api-starter.
---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import express from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import cors from 'cors'
+
+require('dotenv').config()
+
+import { notFound, errorHandler } from './middlewares'
+import api from './api'
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.json({
+    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
+  })
+})
+
+app.use('/api/v1', api)
+
+app.use(notFound)
+app.use(errorHandler)
+
+export default app
